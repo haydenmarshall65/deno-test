fc8c059c9404f79a471f29bc1170c39be8cf6cf4
haydenmarshall65 <hmarshall@altorntl.com>
2024-04-15 10:53:12 -0500

    project idea?

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 export function add(num1: number, num2: number) {
 	return num1 + num2;
 }
+
+//possible uses/personal project: create a small webserver that contains important files to me and can be accessed any time
